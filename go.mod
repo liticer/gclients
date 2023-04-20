fafb37477368bd3779274ea5b554e56b919669fa
Jianhua Shi <jhshi@alauda.io>
2023-04-20 15:22:11 +0800

    Add victoriametrics converter

--- go.mod
+++ go.mod
@@ -1,35 +1,104 @@
 module github.com/liticer/gclients
 
-go 1.17
+go 1.18
 
 require (
 	github.com/gosimple/slug v1.12.0
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/alertmanager v0.24.0
-	github.com/prometheus/client_golang v1.12.1
-	github.com/prometheus/common v0.34.0
+	github.com/prometheus/client_golang v1.14.0
+	github.com/prometheus/common v0.38.0
 )
 
 require (
-	github.com/aws/aws-sdk-go v1.43.11 // indirect
+	github.com/VictoriaMetrics/operator v0.33.0
+	github.com/VictoriaMetrics/operator/api v0.0.0-20230419211903-3fefad006faf
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring v0.64.0
+	gopkg.in/yaml.v2 v2.4.0
+	k8s.io/api v0.27.1
+	k8s.io/apiextensions-apiserver v0.27.1
+	k8s.io/apimachinery v0.27.1
+	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2
+	sigs.k8s.io/controller-runtime v0.14.6
+)
+
+require (
+	github.com/VictoriaMetrics/VictoriaMetrics v1.85.0 // indirect
+	github.com/VictoriaMetrics/fasthttp v1.1.0 // indirect
+	github.com/VictoriaMetrics/metrics v1.23.0 // indirect
+	github.com/VictoriaMetrics/metricsql v0.50.0 // indirect
+	github.com/aws/aws-sdk-go v1.44.157 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/go-kit/log v0.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
+	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/ghodss/yaml v1.0.1-0.20190212211648-25d852aebe32 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.1 // indirect
+	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/go-test/deep v1.0.7 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/gnostic v0.6.9 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/pprof v0.0.0-20221102093814-76f304f74e5e // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/gnostic v0.5.5 // indirect
 	github.com/gosimple/unidecode v1.0.1 // indirect
+	github.com/hashicorp/go-version v1.3.0 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.15.13 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
-	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/onsi/ginkgo/v2 v2.9.1 // indirect
+	github.com/onsi/gomega v1.27.4 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common/sigv4 v0.1.0 // indirect
-	github.com/prometheus/procfs v0.7.3 // indirect
-	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
-	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
-	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/appengine v1.6.6 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fastjson v1.6.3 // indirect
+	github.com/valyala/fastrand v1.1.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/valyala/histogram v1.2.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.3.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/client-go v12.0.0+incompatible // indirect
+	k8s.io/component-base v0.27.1 // indirect
+	k8s.io/klog/v2 v2.90.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20230308215209-15aac26d736a // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.23.1
+	k8s.io/apimachinery => k8s.io/apimachinery v0.23.1
+	k8s.io/client-go => k8s.io/client-go v0.23.1
 )
---
+++ victoriametrics/convert/config.go
@@ -0,0 +1,397 @@
+package converter
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	opConf   *BaseOperatorConf
+	initConf sync.Once
+
+	opNamespace   string
+	initNamespace sync.Once
+)
+
+const prefixVar = "VM"
+const UnLimitedResource = "unlimited"
+
+// WatchNamespaceEnvVar is the constant for env variable WATCH_NAMESPACE
+// which specifies the Namespace to watch.
+// An empty value means the operator is running with cluster scope.
+var WatchNamespaceEnvVar = "WATCH_NAMESPACE"
+
+type Resource struct {
+	Limit struct {
+		Mem string
+		Cpu string
+	}
+	Request struct {
+		Mem string
+		Cpu string
+	}
+}
+
+//genvars:true
+type BaseOperatorConf struct {
+	// enables custom config reloader for vmauth and vmagent,
+	// it should speed-up config reloading process.
+	UseCustomConfigReloader bool `default:"false"`
+	// container registry name prefix, e.g. docker.io
+	ContainerRegistry         string `default:""`
+	CustomConfigReloaderImage string `default:"victoriametrics/operator:config-reloader-v0.32.0"`
+	PSPAutoCreateEnabled      bool   `default:"true"`
+	VMAlertDefault            struct {
+		Image               string `default:"victoriametrics/vmalert"`
+		Version             string `default:"v1.89.1"`
+		Port                string `default:"8080"`
+		UseDefaultResources bool   `default:"true"`
+		Resource            struct {
+			Limit struct {
+				Mem string `default:"500Mi"`
+				Cpu string `default:"200m"`
+			}
+			Request struct {
+				Mem string `default:"200Mi"`
+				Cpu string `default:"50m"`
+			}
+		}
+		ConfigReloaderCPU    string `default:"100m"`
+		ConfigReloaderMemory string `default:"25Mi"`
+		ConfigReloadImage    string `default:"jimmidyson/configmap-reload:v0.3.0"`
+	}
+	VMAgentDefault struct {
+		Image               string `default:"victoriametrics/vmagent"`
+		Version             string `default:"v1.89.1"`
+		ConfigReloadImage   string `default:"quay.io/prometheus-operator/prometheus-config-reloader:v0.58.0"`
+		Port                string `default:"8429"`
+		UseDefaultResources bool   `default:"true"`
+		Resource            struct {
+			Limit struct {
+				Mem string `default:"500Mi"`
+				Cpu string `default:"200m"`
+			}
+			Request struct {
+				Mem string `default:"200Mi"`
+				Cpu string `default:"50m"`
+			}
+		}
+		ConfigReloaderCPU    string `default:"100m"`
+		ConfigReloaderMemory string `default:"25Mi"`
+	}
+
+	VMSingleDefault struct {
+		Image               string `default:"victoriametrics/victoria-metrics"`
+		Version             string `default:"v1.89.1"`
+		Port                string `default:"8429"`
+		UseDefaultResources bool   `default:"true"`
+		Resource            struct {
+			Limit struct {
+				Mem string `default:"1500Mi"`
+				Cpu string `default:"1200m"`
+			}
+			Request struct {
+				Mem string `default:"500Mi"`
+				Cpu string `default:"150m"`
+			}
+		}
+		ConfigReloaderCPU    string `default:"100m"`
+		ConfigReloaderMemory string `default:"25Mi"`
+	}
+
+	VMClusterDefault struct {
+		UseDefaultResources bool `default:"true"`
+		VMSelectDefault     struct {
+			Image    string `default:"victoriametrics/vmselect"`
+			Version  string `default:"v1.89.1-cluster"`
+			Port     string `default:"8481"`
+			Resource struct {
+				Limit struct {
+					Mem string `default:"1000Mi"`
+					Cpu string `default:"500m"`
+				}
+				Request struct {
+					Mem string `default:"500Mi"`
+					Cpu string `default:"100m"`
+				}
+			}
+		}
+		VMStorageDefault struct {
+			Image        string `default:"victoriametrics/vmstorage"`
+			Version      string `default:"v1.89.1-cluster"`
+			VMInsertPort string `default:"8400"`
+			VMSelectPort string `default:"8401"`
+			Port         string `default:"8482"`
+			Resource     struct {
+				Limit struct {
+					Mem string `default:"1500Mi"`
+					Cpu string `default:"1000m"`
+				}
+				Request struct {
+					Mem string `default:"500Mi"`
+					Cpu string `default:"250m"`
+				}
+			}
+		}
+		VMInsertDefault struct {
+			Image    string `default:"victoriametrics/vminsert"`
+			Version  string `default:"v1.89.1-cluster"`
+			Port     string `default:"8480"`
+			Resource struct {
+				Limit struct {
+					Mem string `default:"500Mi"`
+					Cpu string `default:"500m"`
+				}
+				Request struct {
+					Mem string `default:"200Mi"`
+					Cpu string `default:"150m"`
+				}
+			}
+		}
+	}
+
+	VMAlertManager struct {
+		ConfigReloaderImage          string `default:"jimmidyson/configmap-reload:v0.3.0"`
+		ConfigReloaderCPU            string `default:"100m"`
+		ConfigReloaderMemory         string `default:"25Mi"`
+		AlertmanagerDefaultBaseImage string `default:"prom/alertmanager"`
+		AlertManagerVersion          string `default:"v0.24.0"`
+		LocalHost                    string `default:"127.0.0.1"`
+		UseDefaultResources          bool   `default:"true"`
+		Resource                     struct {
+			Limit struct {
+				Mem string `default:"256Mi"`
+				Cpu string `default:"100m"`
+			}
+			Request struct {
+				Mem string `default:"56Mi"`
+				Cpu string `default:"30m"`
+			}
+		}
+	}
+
+	DisableSelfServiceScrapeCreation bool `default:"false"`
+	VMBackup                         struct {
+		Image               string `default:"victoriametrics/vmbackupmanager"`
+		Version             string `default:"v1.89.1-enterprise"`
+		Port                string `default:"8300"`
+		UseDefaultResources bool   `default:"true"`
+		Resource            struct {
+			Limit struct {
+				Mem string `default:"500Mi"`
+				Cpu string `default:"500m"`
+			}
+			Request struct {
+				Mem string `default:"200Mi"`
+				Cpu string `default:"150m"`
+			}
+		}
+		LogLevel  string `default:"INFO"`
+		LogFormat string
+	}
+	VMAuthDefault struct {
+		Image               string `default:"victoriametrics/vmauth"`
+		Version             string `default:"v1.89.1"`
+		ConfigReloadImage   string `default:"quay.io/prometheus-operator/prometheus-config-reloader:v0.48.1"`
+		Port                string `default:"8427"`
+		UseDefaultResources bool   `default:"true"`
+		Resource            struct {
+			Limit struct {
+				Mem string `default:"300Mi"`
+				Cpu string `default:"200m"`
+			}
+			Request struct {
+				Mem string `default:"100Mi"`
+				Cpu string `default:"50m"`
+			}
+		}
+		ConfigReloaderCPU    string `default:"100m"`
+		ConfigReloaderMemory string `default:"25Mi"`
+	}
+
+	EnabledPrometheusConverter struct {
+		PodMonitor         bool `default:"true"`
+		ServiceScrape      bool `default:"true"`
+		PrometheusRule     bool `default:"true"`
+		Probe              bool `default:"true"`
+		AlertmanagerConfig bool `default:"true"`
+	}
+	FilterChildLabelPrefixes      []string `default:""`
+	FilterChildAnnotationPrefixes []string `default:""`
+	// adds compare-options and sync-options for prometheus objects converted by operator
+	// it helps to properly use converter with ArgoCD
+	PrometheusConverterAddArgoCDIgnoreAnnotations bool `default:"false"`
+	EnabledPrometheusConverterOwnerReferences     bool `default:"false"`
+	// allows filtering for converted labels, labels with matched prefix will be ignored
+	FilterPrometheusConverterLabelPrefixes []string `default:""`
+	// allows filtering for converted annotations, annotations with matched prefix will be ignored
+	FilterPrometheusConverterAnnotationPrefixes []string `default:""`
+	Host                                        string   `default:"0.0.0.0"`
+	ListenAddress                               string   `default:"0.0.0.0"`
+	DefaultLabels                               string   `default:"managed-by=vm-operator"`
+	Labels                                      Labels   `ignored:"true"`
+	LogLevel                                    string
+	LogFormat                                   string
+	ClusterDomainName                           string        `default:""`
+	PodWaitReadyTimeout                         time.Duration `default:"80s"`
+	PodWaitReadyIntervalCheck                   time.Duration `default:"5s"`
+	PodWaitReadyInitDelay                       time.Duration `default:"10s"`
+	// configures force resync interval for VMAgent, VMAlert and VMAlertmanager
+	ForceResyncInterval time.Duration `default:"60s"`
+}
+
+// Validate - validates config on best effort.
+func (boc BaseOperatorConf) Validate() error {
+	validateResource := func(name string, res Resource) error {
+		if res.Request.Mem != UnLimitedResource {
+			if _, err := resource.ParseQuantity(res.Request.Mem); err != nil {
+				return fmt.Errorf("cannot parse resource request memory for %q, err :%w", name, err)
+			}
+		}
+		if res.Request.Cpu != UnLimitedResource {
+			if _, err := resource.ParseQuantity(res.Request.Cpu); err != nil {
+				return fmt.Errorf("cannot parse resource request cpu for %q, err :%w", name, err)
+			}
+		}
+		if res.Limit.Mem != UnLimitedResource {
+			if _, err := resource.ParseQuantity(res.Limit.Mem); err != nil {
+				return fmt.Errorf("cannot parse resource limit memory for %q, err :%w", name, err)
+			}
+		}
+		if res.Limit.Cpu != UnLimitedResource {
+			if _, err := resource.ParseQuantity(res.Limit.Cpu); err != nil {
+				return fmt.Errorf("cannot parse resource limit cpu for %q, err :%w", name, err)
+			}
+		}
+		return nil
+	}
+	if err := validateResource("vmagent", Resource(boc.VMAgentDefault.Resource)); err != nil {
+		return err
+	}
+	if err := validateResource("vmalert", Resource(boc.VMAlertDefault.Resource)); err != nil {
+		return err
+	}
+	if err := validateResource("vmalertmanager", Resource(boc.VMAlertManager.Resource)); err != nil {
+		return err
+	}
+	if err := validateResource("vmselect", Resource(boc.VMClusterDefault.VMSelectDefault.Resource)); err != nil {
+		return err
+	}
+	if err := validateResource("vminsert", Resource(boc.VMClusterDefault.VMInsertDefault.Resource)); err != nil {
+		return err
+	}
+	if err := validateResource("vmstorage", Resource(boc.VMClusterDefault.VMStorageDefault.Resource)); err != nil {
+		return err
+	}
+
+	if err := validateResource("vmsingle", Resource(boc.VMSingleDefault.Resource)); err != nil {
+		return err
+	}
+	if err := validateResource("vmbackup", Resource(boc.VMBackup.Resource)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func MustGetBaseConfig() *BaseOperatorConf {
+	initConf.Do(func() {
+		c := &BaseOperatorConf{}
+		err := envconfig.Process(prefixVar, c)
+		if err != nil {
+			panic(err)
+		}
+		if c.DefaultLabels != "" {
+			defL := Labels{}
+			err := defL.Set(c.DefaultLabels)
+			if err != nil {
+				panic(err)
+			}
+			c.Labels = defL
+		}
+		if err := c.Validate(); err != nil {
+			panic(err)
+		}
+		opConf = c
+	})
+	return opConf
+}
+
+var validNamespaceRegex = regexp.MustCompile(`[a-z0-9]([-a-z0-9]*[a-z0-9])?`)
+
+func getWatchNamespace() (string, error) {
+	wns, _ := os.LookupEnv(WatchNamespaceEnvVar)
+	if len(wns) > 0 {
+		// validate namespace with regexp
+		if !validNamespaceRegex.MatchString(wns) {
+			return "", fmt.Errorf("incorrect value: %s for env var %s, it must match regex: %s", wns, WatchNamespaceEnvVar, validNamespaceRegex.String())
+		}
+	}
+	return wns, nil
+}
+
+// MustGetWatchNamespace returns the Namespace the operator should be watching for changes
+func MustGetWatchNamespace() string {
+	initNamespace.Do(func() {
+		wns, err := getWatchNamespace()
+		if err != nil {
+			panic(err)
+		}
+		opNamespace = wns
+	})
+
+	return opNamespace
+}
+
+func MustGetNamespaceListOptions() *client.ListOptions {
+	return &client.ListOptions{
+		Namespace: MustGetWatchNamespace(),
+	}
+}
+
+type Labels struct {
+	LabelsString string
+	LabelsMap    map[string]string
+}
+
+// Implement the flag.Value interface
+func (labels *Labels) String() string {
+	return labels.LabelsString
+}
+
+// Merge labels create a new map with labels merged.
+func (labels *Labels) Merge(otherLabels map[string]string) map[string]string {
+	mergedLabels := map[string]string{}
+
+	for key, value := range otherLabels {
+		mergedLabels[key] = value
+	}
+
+	for key, value := range labels.LabelsMap {
+		mergedLabels[key] = value
+	}
+	return mergedLabels
+}
+
+// Set implements the flag.Set interface.
+func (labels *Labels) Set(value string) error {
+	m := map[string]string{}
+	if value != "" {
+		splited := strings.Split(value, ",")
+		for _, pair := range splited {
+			sp := strings.Split(pair, "=")
+			m[sp[0]] = sp[1]
+		}
+	}
+	labels.LabelsMap = m
+	labels.LabelsString = value
+	return nil
+}
